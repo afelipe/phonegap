c168a11333902293210d9d4fc291a93c088e213f
User <user@MacBook-Pro-de-User.local>
2012-12-02 21:24:41 +0100

    second commit

--- js/main.js
+++ js/main.js
@@ -1,15 +1,6 @@
 var app = {
 
-findByName: function() {
-    var self = this;
-    this.store.findByName($('.search-key').val(), function(employees) {
-                          $('.employee-list').html(self.employeeLiTpl(employees));
-                          });
-},
-    renderHomeView: function() {
-        $('body').html(this.homeTpl());
-        $('.search-key').on('keyup', $.proxy(this.findByName, this));
-    },
+   
         showAlert: function (message, title) {
             if (navigator.notification) {
                 navigator.notification.alert(message, null, title, 'OK');
@@ -17,17 +8,12 @@ findByName: function() {
                 alert(title ? (title + ": " + message) : message);
             }
         },
-    initialize: function() {
-        var self = this;
-        this.store = new MemoryStore(function() {
-                                     self.renderHomeView();
-                                     });
-        this.homeTpl = Handlebars.compile($("#home-tpl").html());
-        this.employeeLiTpl = Handlebars.compile($("#employee-li-tpl").html());
-        //$('.search-key').on('keyup', $.proxy(this.findByName, this));
-        //this.store = new MemoryStore();
-       
-    }
+   initialize: function() {
+    var self = this;
+    this.store = new MemoryStore(function() {
+        $('body').html(new HomeView(self.store).render().el);
+    });
+}
 
 };
 
